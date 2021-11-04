a5884b2bfabeaca28890a1d70832ef34ce302cf4
zied lazrak <zied.lazrak@esprit.tn>
2021-11-04 21:58:01 +0000

    fix bugs

--- src/main/java/tn/esprit/spring/controller/ControllerEntrepriseImpl.java
+++ src/main/java/tn/esprit/spring/controller/ControllerEntrepriseImpl.java
@@ -34,7 +34,7 @@ public class ControllerEntrepriseImpl{
 	}
 	public Entreprise getEntrepriseById(int entrepriseId) {
 
-		return ientrepriseservice.getEntrepriseById(1);
+		return ientrepriseservice.getEntrepriseById(entrepriseId);
 	}
 	
 	public int ajouterDepartement(Departement dep) {
--- src/main/java/tn/esprit/spring/controller/RestControlEmploye.java
+++ src/main/java/tn/esprit/spring/controller/RestControlEmploye.java
@@ -34,8 +34,6 @@ public class RestControlEmploye {
 
 	
 	// http://localhost:8081/SpringMVC/servlet/ajouterEmployer
-	//{"id":1,"nom":"kallel", "prenom":"khaled", "email":"Khaled.kallel@ssiiconsulting.tn", "isActif":true, "role":"INGENIEUR"}
-	  
 	@PostMapping("/ajouterEmployer")
 	@ResponseBody
 	public Employe ajouterEmploye(@RequestBody Employe employe)
@@ -66,7 +64,6 @@ public class RestControlEmploye {
 	}
 
 	// http://localhost:8081/SpringMVC/servlet/ajouterContrat
-	//{"reference":6,"dateDebut":"2020-03-01","salaire":2000,"typeContrat":"CDD"}
 	@PostMapping("/ajouterContrat")
 	@ResponseBody
 	public int ajouterContrat(@RequestBody Contrat contrat) {
