26eab7fb76f12811467b85c4aa7e6e5ccc6ea5ff
Ipharmacy <dhia.benhamouda@esprit.tn>
2021-11-14 20:42:50 +0100

    fix null issues

--- src/main/java/tn/esprit/spring/services/IEntrepriseService.java
+++ src/main/java/tn/esprit/spring/services/IEntrepriseService.java
@@ -7,8 +7,8 @@ import tn.esprit.spring.entities.Entreprise;
 
 public interface IEntrepriseService {
 	
-	public int ajouterEntreprise(Entreprise entreprise);
-	public int ajouterDepartement(Departement dep);
+	public Entreprise ajouterEntreprise(Entreprise entreprise);
+	public Departement ajouterDepartement(Departement dep);
 	void affecterDepartementAEntreprise(int depId, int entrepriseId);
 	List<String> getAllDepartementsNamesByEntreprise(int entrepriseId);
 	public void deleteEntrepriseById(int entrepriseId);
